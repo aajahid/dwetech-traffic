79f2b1c3609b52648813b2477ee5e3b6f354052a
ridwanul Hafiz <ridwanul.hafiz@gmail.com>
2013-08-31 14:52:54 +0600

    ok

---
+++ include/header.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ridwa_000
+ * Date: 8/31/13
+ * Time: 2:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Ridwa_000
+ * Date: 8/31/13
+ * Time: 2:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
